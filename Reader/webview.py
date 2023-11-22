d66fac89fb8a5305001f722c921be41d7e1f7791
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-22 17:15:17 +0530

    Create webview.py

---
+++ Reader/webview.py
@@ -0,0 +1,17 @@
+
+from tkinterweb import HtmlFrame
+import tkinter as tk
+
+root = tk.Tk()
+frame = HtmlFrame(root)
+
+username = 'imvickykumar999'
+password = 'imvickykumar999'
+ip = '192.168.0.102'
+port = 8080
+
+url = f"http://{username}:{password}@{ip}:{port}/sensors.json"
+frame.load_website(url)
+
+frame.pack(fill="both", expand=True)
+root.mainloop()
