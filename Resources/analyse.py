cf883bf976189e6a48a1393c2800a5ae49c07824
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-22 19:42:18 +0530

    Update analyse.py

--- Resources/analyse.py
+++ Resources/analyse.py
@@ -8,18 +8,27 @@ with open(filename, 'r') as f:
     data = json.load(f)
 
 keys = list(data.keys())
+print()
 
 for i, j in enumerate(keys):
     print(i, j)
 
-print(data[keys[9]])
+battery_level = data[keys[9]]
+print()
 
-ts = int("1700651666606")
-ts /= 1000
-print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+def convert(ts):
+    ts /= 1000
+    unix = datetime.utcfromtimestamp(ts)
+    mili = unix.strftime('%Y-%m-%d %H:%M:%S')
+    return mili
+
+for i in battery_level['data']:
+    print(i[1][0], battery_level['unit'], '@', convert(int(i[0])))
 
 
-'''
+rf'''
+>>> python analyse.py
+
 0 accel
 1 battery_charging
 2 mag
@@ -32,7 +41,7 @@ print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
 9 battery_level
 10 battery_temp
 
-{'unit': '%', 'data': [[1700651664604, [77.0]], [1700651666606, [77.0]], [1700651668609, [77.0]]]}
-
-2023-11-22 11:14:24
+77.0 % @ 2023-11-22 11:14:24
+77.0 % @ 2023-11-22 11:14:26
+77.0 % @ 2023-11-22 11:14:28
 '''
