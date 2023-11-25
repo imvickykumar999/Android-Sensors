700b2769cae3ee7e6a3a456a2e0a87608e8e9848
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-25 17:59:06 +0530

    sensor

---
+++ SensorServer/sensor.py
@@ -0,0 +1,34 @@
+
+# https://github.com/umer0586/SensorServer#example-websocket-client-python
+
+import websocket
+import json
+
+def on_message(ws, message):
+    values = json.loads(message)['values']
+    x = values[0]
+    y = values[1]
+    z = values[2]
+    print("x = ", x , "y = ", y , "z = ", z )
+
+def on_error(ws, error):
+    print("error occurred ", error)
+    
+def on_close(ws, close_code, reason):
+    print("connection closed : ", reason)
+    
+def on_open(ws):
+    print("connected")
+    
+
+def connect(url):
+    ws = websocket.WebSocketApp(url,
+                              on_open=on_open,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close)
+
+    ws.run_forever()
+ 
+ 
+connect("ws://192.168.0.103:8080/sensor/connect?type=android.sensor.accelerometer") 
